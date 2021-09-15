4b0e95b7658f8a2c20c48fbd8c9836c5e0b7286d
DimasYl <dima.ilyushin.2016@mail.ru>
2021-09-15 19:40:07 +0300

    Updates

---
+++ precache-manifest.6d0eb189205f7d237089e9600c1ea911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496db2b5292ffe5d14b6b9d37bdfc6ca",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "0ec481d3a7c5454d8230",
+    "url": "/todolist/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4b33b41a03bebeb8c4da",
+    "url": "/todolist/static/js/2.a3580e32.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.a3580e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa1351503f20a14dc04",
+    "url": "/todolist/static/js/3.bd268246.chunk.js"
+  },
+  {
+    "revision": "0ec481d3a7c5454d8230",
+    "url": "/todolist/static/js/main.75ae84f4.chunk.js"
+  },
+  {
+    "revision": "5833fb5700cfadc9c7e6",
+    "url": "/todolist/static/js/runtime-main.b0e59d38.js"
+  }
+]);
